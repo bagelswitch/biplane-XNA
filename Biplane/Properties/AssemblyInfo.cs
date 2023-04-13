624814474091971e7a00921b4cdb8ee90dc35124
bagelswitch <n_s_cohen@hotmail.com>
2023-04-12 20:51:56 -0700

    migrating to github

---
+++ Biplane/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Biplane")]
+[assembly: AssemblyProduct("Biplane")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Tyrell Corporation")]
+
+[assembly: AssemblyCopyright("Copyright © Tyrell Corporation 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd309f5b-0804-497c-9e51-c2c540d007e8")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
